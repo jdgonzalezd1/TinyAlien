596fbd7c489ab027562bfa46511410c2cc4d8fb8
Damamapi <ddamamapi@gmail.com>
2023-09-13 12:07:16 -0500

    Spawn Objects on Start

--- Assets/Scripts/Object Spawner.cs
+++ Assets/Scripts/Object Spawner.cs
@@ -9,30 +9,50 @@ public class ObjectSpawner : MonoBehaviour
     public GameObject character;
     float xMaxDistance,  zMaxDistance;
     float xMinDistance, zMinDistance;
+    
+    const float FOODSIZE = 5f;
+    const float XMIN = 2f , ZMIN = 2f;
+    const float XMAX = 20f, ZMAX = 20f;
+
 
 
 
     // Start is called before the first frame update
     void Start()
     {
+        TrackCharacterPosition();
         for (int i = 0; i < 10; i++)
         {
-            Vector3 spawnPosition = new Vector3()
+            float xPosition = Random.Range(-xMaxDistance, xMaxDistance);
+            float zPosition = Random.Range(-zMaxDistance, zMaxDistance);
+
+            if (xPosition < xMinDistance && xPosition > -xMinDistance) xPosition += xMinDistance;
+            if (zPosition < zMinDistance && zPosition > -zMinDistance) zPosition += zMinDistance;
+
+            Vector3 spawnPosition = new Vector3(xPosition, character.transform.position.y + 3f, zPosition);
+            PickAndSpawn(spawnPosition, Quaternion.identity);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        xMinDistance = character.transform.position.x + 5f;
-        xMaxDistance = character.transform.position.x + 30f;
-        zMinDistance = character.transform.position.z + 5f;
-        zMaxDistance = character.transform.position.z + 30f;
+        TrackCharacterPosition();
     }
 
-    void PickAndSpawn(Vector3 positionToSpawn, Quaternion rotationToSpawn)
+    private void PickAndSpawn(Vector3 positionToSpawn, Quaternion rotationToSpawn)
     {
         int randomIndex = Random.Range(0, itemsToPickFrom.Length);
         GameObject clone = Instantiate(itemsToPickFrom[randomIndex], positionToSpawn, rotationToSpawn);
+        clone.transform.localScale = new Vector3(FOODSIZE, FOODSIZE, FOODSIZE);
+        clone.transform.parent = transform;
+    }
+
+    private void TrackCharacterPosition()
+    {
+        xMinDistance = character.transform.position.x + XMIN;
+        xMaxDistance = character.transform.position.x + XMAX;
+        zMinDistance = character.transform.position.z + ZMIN;
+        zMaxDistance = character.transform.position.z + ZMAX;
     }
 }
