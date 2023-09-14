fb06ea6bdadea6ca39fe3b7c2bb78260074b25af
Mario Andres Henao <mariodeoz-123@hotmail.com>
2023-09-14 13:37:55 -0500

    Main_Menu And InGame UI
    Creation of the main menu, the ingame UI and the pause menu.
    Assets Medium Size.

---
+++ Assets/Scripts/PauseMenu_Script.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu_Script : MonoBehaviour
+{
+
+    public GameObject pauseMenuUI;
+
+    public void Start()
+    {
+        pauseMenuUI.SetActive(false);
+    }
+
+    public void Update()
+    {
+        //Pause the game, and show the mouse cursor
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(pauseMenuUI.activeInHierarchy == false)
+            {
+                pauseMenuUI.SetActive(true);
+                Cursor.lockState = CursorLockMode.None;
+            }
+            else
+            {
+                Resume();
+            }
+        }
+    }
+
+    public void TryAgain()
+    {
+        //Reset the game when the player fail or die.
+    }
+
+    //unpause the game, and hide the mouse cursor 
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+
+    //send back to the main menu
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
