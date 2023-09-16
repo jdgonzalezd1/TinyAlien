594bba57af1351f16ccbd467bc3aa974448fd4d1
jdgonzalezd1 <60698448+jdgonzalezd1@users.noreply.github.com>
2023-09-16 16:09:24 -0500

    Missile spawner update

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -5,23 +5,31 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] GameObject enemy;
+    [SerializeField] PlayerController playerController;
 
     Vector3 spawnStreet;
+    public int stageEnable;
     public float spawnPosX, spawnPosZ;
-
+    public float spawnInterval = 5.0f;
     float startDelay = 1.0f;
-    float spawnInterval = 3.0f;
     
+
     void Start()
     {
+        playerController = FindAnyObjectByType<PlayerController>();
         InvokeRepeating("SpawnMissile", startDelay, spawnInterval);
     }
 
     void SpawnMissile()
     {
-        spawnStreet = new Vector3(spawnPosX, 1, spawnPosZ);
-        Instantiate(enemy, spawnStreet, enemy.transform.rotation);
+        if (stageEnable <= playerController.stage)
+        {
+            Debug.Log("Enable = " + stageEnable + " ---- Stage = " + playerController.stage);
+            spawnStreet = new Vector3(spawnPosX, 1, spawnPosZ);
+            Instantiate(enemy, spawnStreet, enemy.transform.rotation);
+        }
     }
+
 
 
 
